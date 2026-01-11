60cd87e13ca3cd0486fc220ddbfe3c0840c2b75a
Priti <priti16maurya@gmail.com>
2026-01-12 00:30:48 +0530

    Dashboard holdings and Positions

--- dashboard/src/components/Holdings.js
+++ dashboard/src/components/Holdings.js
@@ -1,9 +1,10 @@
 import React from "react";
+import {holdings} from  "../data/data";
 
 const Holdings = () => {
   return (
     <>
-      <h3 className="title">Holdings (13)</h3>
+      <h3 className="title">Holdings ({holdings.length})</h3>
 
       <div className="order-table">
         <table>
@@ -17,6 +18,24 @@ const Holdings = () => {
             <th>Net chg.</th>
             <th>Day chg.</th>
           </tr>
+          {holdings.map((stock,index)=>{
+            const currVal = stock.price*stock.qty;
+            const isProfit = currVal - stock.avg*stock.qty >=0.0;
+            const profitClass = isProfit ? "profit" : "loss";
+            const dayClass = stock.isLoss? "loss" : "profit";
+            return (
+            <tr key={index} className="items"> 
+            <td>{stock.name}</td>
+            <td>{stock.qty}</td>
+            <td>{stock.avg.toFixed(2)}</td>
+            <td>{stock.price.toFixed(2)}</td>
+            <td>{currVal.toFixed(2)}</td>
+            <td className={profitClass}>{(currVal - stock.avg*stock.qty).toFixed(2)}</td>
+            <td className={profitClass}>{stock.net}.</td>
+            <td className={dayClass}>{dayClass}.</td>
+          </tr>
+            )
+          })}
         </table>
       </div>
 
--- dashboard/src/components/Positions.js
+++ dashboard/src/components/Positions.js
@@ -1,9 +1,9 @@
 import React from "react";
-
+import {positions} from  "../data/data";
 const Positions = () => {
   return (
     <>
-      <h3 className="title">Positions (2)</h3>
+      <h3 className="title">Positions ({positions.length})</h3>
 
       <div className="order-table">
         <table>
@@ -16,6 +16,23 @@ const Positions = () => {
             <th>P&L</th>
             <th>Chg.</th>
           </tr>
+          {positions.map((stock,index)=>{
+                      const currVal = stock.price*stock.qty;
+                      const isProfit = currVal - stock.avg*stock.qty >=0.0;
+                      const profitClass = isProfit ? "profit" : "loss";
+                      const dayClass = stock.isLoss? "loss" : "profit";
+                      return (
+                      <tr key={index} className="items"> 
+                      <td>{stock.product}</td>
+                      <td>{stock.name}</td>
+                      <td>{stock.qty}</td>
+                      <td>{stock.avg.toFixed(2)}</td>
+                      <td>{currVal.toFixed(2)}</td>
+                      <td className={profitClass}>{(currVal - stock.avg*stock.qty).toFixed(2)}</td>
+                      <td className={dayClass}>{dayClass}.</td>
+                    </tr>
+                      )
+                    })}
         </table>
       </div>
     </>
