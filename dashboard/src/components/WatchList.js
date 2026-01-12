8246b7201154a032f5adc3d8f86199b99a07c4ce
Priti <priti16maurya@gmail.com>
2026-01-12 21:53:35 +0530

    dashboard front end GUI Completed

--- dashboard/src/components/WatchList.js
+++ dashboard/src/components/WatchList.js
@@ -1,4 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+import {Tooltip,Grow} from "@mui/material"
+import {watchlist} from "../data/data"
+//import WatchListItem from "./WatchListItem";
+import {BarChartOutlined, KeyboardArrowDown,KeyboardArrowUp, MoreHoriz} from "@mui/icons-material";
 
 const WatchList = () => {
   return (
@@ -11,12 +15,69 @@ const WatchList = () => {
           placeholder="Search eg:infy, bse, nifty fut weekly, gold mcx"
           className="search"
         />
-        <span className="counts"> 9 / 50</span>
+        <span className="counts"> {watchlist.length} / 50</span>
       </div>
 
-      <ul className="list"></ul>
+      <ul className="list">
+        {watchlist.map((stock,index)=>{
+          return <WatchListItem stock={stock} key ={index} />
+          
+        })}
+      </ul>
     </div>
   );
 };
 
 export default WatchList;
+
+const WatchListItem =({stock})=>{
+  const [showWatchListAction, setShowWatchListAction] = useState(false);
+  const handleMouseEnter =(e) =>{
+    setShowWatchListAction(true);
+  }
+  const handleMouseLeave = (e) =>{
+    setShowWatchListAction(false);
+  }
+  return (
+    <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      <div className="item">
+        <p className={stock.isDown ? "down" : "up"}>{stock.name}</p>
+        <div className="itemInfo">
+          <span className="percent">{stock.percent}</span>
+          { stock.isDown ? ( 
+          <KeyboardArrowDown className= "down" />) : 
+          <KeyboardArrowUp className="up" />
+          }
+           <span className="price">{stock.price}</span>
+        </div>
+      </div>
+      {showWatchListAction && <WatchListAction uid={stock.name} />}
+    </li>
+  )
+}
+
+const WatchListAction =({uid})=>{
+  return(
+    <span className="actions">
+      <span>
+        <Tooltip titile="BUY (B)" placment="top" arrow TransitionComponent={Grow}>
+          <button className="buy">Buy</button>
+        </Tooltip>
+        <Tooltip titile="SELL (S)" placment="top" arrow TransitionComponent={Grow}>
+          <button className="sell">Sell</button>
+        </Tooltip>
+        <Tooltip titile="Analytics (A)" placment="top" arrow TransitionComponent={Grow}>
+          <button className="action">
+            <BarChartOutlined className="icon" />
+          </button>
+        </Tooltip>
+        <Tooltip titile="More" placment="top" arrow TransitionComponent={Grow}>
+          <button className="action">
+            <MoreHoriz className="icon" />
+          </button>
+        </Tooltip>
+      </span>
+    </span>
+  )
+
+}
---
+++ dashboard/src/components/WatchListItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function WatchListItem() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default WatchListItem
