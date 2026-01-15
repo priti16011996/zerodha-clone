db0de4d89dba8ec27a4648419e849ec143f9f332
Priti <priti16maurya@gmail.com>
2026-01-15 23:43:12 +0530

    set env and connect to mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,15 @@
 const express  = require('express');
+require("dotenv").config();
+const mongoose = require('mongoose')
+
 const app = express();
-const PORT = 3000;
+
+const PORT = process.env.PORT || 3000;
+const MONGO_URL = process.env.MONGO_URL;
+
 
 app.listen(PORT,(req,res)=>{
     console.log(`Server is running on port ${PORT}`);
-})
+    mongoose.connect(MONGO_URL);
+    console.log("Connection Successful to the Database");
+});
